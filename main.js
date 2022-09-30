c21864cabedac4c05ee55726a7e054f9d2c1743a
sunridden <kevinszeto9449@gmail.com>
2022-09-29 21:42:22 -0700

    Refactored code for game logic

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const Player = (piece) => {
-    let isAlive = true;
+
     let turn = false;
 
     return {
-        isAlive, turn, piece
+        turn, piece
     };
 };
 
@@ -13,8 +13,9 @@ const player2 = Player("O");
 const gameController = (() => {
 
     const squares = document.querySelectorAll(".square");
-
+    const displayText = document.getElementById("winner");
     const restartBtn = document.getElementById("restart-btn");
+
     restartBtn.addEventListener('click', () => {
         trackInput();
     })
@@ -29,23 +30,29 @@ const gameController = (() => {
 
     function btnInput()  {
         
-        //indexes the clicked square and updates gameboard array
         let currentTurn = changeTurn();
+        displayTurn(turn);
+    
         let squarePosition = this.className.split(" ")[0];
-        gameBoard.gameboard.splice(squarePosition, 1, currentTurn);
+        gameBoard.updateBoard(squarePosition, currentTurn);
+
         this.textContent = currentTurn;
         
         //locks the tile after spot is taken
         this.removeEventListener('click', btnInput);
 
-        //calls findWinner function
         let winner = findWinner(squarePosition, currentTurn);
-        if (winner != undefined || isGameOver() === true) {
+
+        if (winner.length != 0 || isGameOver() === true) {
             //makes a call that the game is over
             squares.forEach(square => {
                 square.removeEventListener('click', btnInput);
             })
-            displayWinner(winner);
+            if (winner.length == 0 && isGameOver() === true) {
+                displayWinner("Tie");
+            } else {
+                displayWinner(currentTurn);
+            }
         }
     }
 
@@ -53,19 +60,16 @@ const gameController = (() => {
         squares.forEach(square => {
             square.textContent = "";
             square.removeEventListener('click', btnInput);
-
         })
 
-        const winnerText = document.getElementById("winner");
-        winnerText.textContent = "Dummy text";
+        displayTurn();
         
-       
         gameBoard.gameboard.fill(0);
     }
     
     //initializes player1 to start first
     let turn = 1;
-
+    
     const changeTurn = () => {
         
         switch(turn) {
@@ -88,6 +92,42 @@ const gameController = (() => {
         }
     }
 
+    const findWinner = (squareIndex, currentTurn) => {
+
+        squareIndex = Number(squareIndex);
+    
+        const winningCombinations = [
+        [0, 3, 6], //col1
+        [1, 4 ,7], //col2
+        [2, 5, 8], //col3
+        [0, 1, 2], //row1
+        [3, 4, 5], //row2
+        [6, 7, 8], //row3
+        [0, 4, 8], //diagonal1
+        [2, 4, 6], //disagonal2
+        ]
+
+        let winner = winningCombinations.filter((arr) => {
+            if (arr.includes(squareIndex)) {
+                return true;
+            } else {
+                return false;
+            }
+        })
+        
+        winner = winner.filter((arr) => {
+            let winStatus = true;
+            arr.forEach(index => {
+                if (gameBoard.gameboard[index] != currentTurn) {
+                    winStatus = false;
+                }
+            })
+            return winStatus;
+        })
+
+        return winner;
+    }
+
     const isGameOver = () => {
         
         let gameStatus = true;
@@ -97,120 +137,42 @@ const gameController = (() => {
                 gameStatus = false;
             }
         })
-        
-        if (gameStatus === true) {
-            return true;
+
+        return gameStatus;
+    }
+
+    const displayTurn = () => {
+        displayText.textContent = ("Player " + turn + " 's Turn");
+    }
+
+    const displayWinner = (winner) => {
+        if (winner === "Tie") {
+            displayText.textContent = "There is a tie!"
         } else {
-            return false;
+            displayText.textContent = ("Player " + winner + " wins!");
         }
     }
 
     return {
-        changeTurn, isGameOver
+        changeTurn, isGameOver, findWinner
     };
 })();
 
 const gameBoard = (() => {
 
-    const gameboard = [0, 0, 0, 0, 0, 0, 0, 0];
+    const gameboard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    const updateBoard = (index, turn) => {
+        gameboard.splice(index, 1, turn);
+    }
 
     return {
-        gameboard
+        gameboard, updateBoard
     };
 })();
 
-function findWinner(squareIndex, currentTurn) {
 
-    squareIndex = Number(squareIndex);
 
-    let colWinner = false;
-    let rowWinner = false;
-    let diagonalWinner = false;
 
-    //possible winning indexes 
-    let col1 = [0, 3, 6];
-    let col2 = [1, 4 ,7];
-    let col3 = [2, 5, 8];
 
-    let row1 = [0, 1, 2];
-    let row2 = [3, 4, 5];
-    let row3 = [6, 7, 8];
 
-    let diagonal1 = [0, 4, 8];
-    let diagonal2 = [2, 4, 6];
-
-    switch(true) {
-        case col1.includes(squareIndex):
-            colWinner = iterate(col1);
-            break;
-        case col2.includes(squareIndex):
-            colWinner = iterate(col2);
-            break;
-        case col3.includes(squareIndex):
-            colWinner = iterate(col3);
-            break;
-    }
-
-    if (colWinner === true) {
-        return currentTurn;
-    }
-
-    switch(true) {
-        case row1.includes(squareIndex):
-            rowWinner = iterate(row1);
-            break;
-        case row2.includes(squareIndex):
-            rowWinner = iterate(row2);
-            break;
-        case row3.includes(squareIndex):
-            rowWinner = iterate(row3);
-            break;
-    }
-
-    if (rowWinner === true) {
-        return currentTurn;
-    }
-
-    switch(true) {
-        case diagonal1.includes(squareIndex):
-            diagonalWinner = iterate(diagonal1);
-            break;
-        case diagonal2.includes(squareIndex):
-            diagonalWinner = iterate(diagonal2);
-            break;
-    }
-
-    if (diagonalWinner === true) {
-        return currentTurn;
-    } else if (gameController.isGameOver() === true) {
-        return ("Tie");
-    }
-
-    function iterate(group) {
-        let winner = true;
-        group.forEach(element => {
-            //checks if the element at each index equals currentTurn
-            if (gameBoard.gameboard[element] != currentTurn) {
-                winner = false;
-            }
-        })
-
-        if (winner === true) {
-            console.log("Winner at " + group);
-            return true;
-        } else {
-            return false;
-        }
-    }
-}
-
-function displayWinner(winner) {
-    const winnerText = document.getElementById("winner");
-    if (winner === "Tie") {
-        winner.textContent = "There is a tie!"
-    } else {
-        winnerText.textContent = ("Player " + winner + " wins!");
-    }
-}
-
-gameController.startGame();
